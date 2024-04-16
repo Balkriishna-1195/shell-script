be11888a053dc471ad1ec00b4dc9dd3c451a8dc1
Balkriishna-1195 <venatibalakrishna1195@gmail.com>
2024-04-16 17:05:39 +0530

    hello-message

--- 10-installation.sh
+++ 10-installation.sh
@@ -10,3 +10,22 @@ else
    echo "You are super user"
 fi
 
+dnf install mysql -y
+
+if [ $? -ne 0 ]
+then 
+    echo "installation of mysql... failure"
+    exit 1
+else
+    echo "onstallation of mysql...success"
+fi
+dnf install git -y
+
+if [ $? -ne 0 ]
+then 
+    echo "installation of git... failure"
+    exit 1
+else
+    echo "onstallation of git...success"
+fi
+
