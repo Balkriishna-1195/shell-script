a618ff086ea4117475c2ade94fe0a1458e5790d3
Balkriishna-1195 <venatibalakrishna1195@gmail.com>
2024-04-16 16:28:25 +0530

    hello-message

---
+++ 02-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh::Hello suresh, How are you?"
+echo "Suresh::Hi ramesh, I am fine. How are you?"
+echo "Ramesh:: I am fine too. How is Your work?"
+echo "Suresh:: not bad. I am thinking to upgrade to Devops"
