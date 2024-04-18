a9df0cf9e699eff48d3061a76de4f5c649640448
Balkriishna-1195 <venatibalakrishna1195@gmail.com>
2024-04-18 14:58:28 +0530

    functions adding

---
+++ 11-functions.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+        echo "$2 ....failure"
+        exit 1
+    else
+        echo "$2 .....Success"
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root access"
+    exit 1
+else 
+   echo "You are super user"
+fi
+
+dnf install mysql -y
+VALIDATE $? "installing mysql"
+
+dnf install git -y
+VALIDATE $? "installing git"
+
+
