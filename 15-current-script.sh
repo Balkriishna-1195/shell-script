9bb94fdd0b08ebf1d3e7a3f4cb1c56f648141c93
Balkriishna-1195 <venatibalakrishna1195@gmail.com>
2024-04-24 11:46:57 +0530

    expense shell my sql

--- 15-current-script.sh
+++ 15-current-script.sh
@@ -4,6 +4,8 @@ COURSE="Devops from current script"
 echo "Before calling other script, course: $COURSE"
 echo "process iD of current script : $$"
 
-./16-other-script.sh
+#./16-other-script.sh
+
+source ./16-other-script.sh
 
 echo "After calling other script, course: $COURSE"
